04933ecc6d938aa59549cc0cdd852756510833b8
NiklasRenner <NiklasRennerNielsen@gmail.com>
2016-06-18 17:22:22 +0200

    added functionality

--- android-app/app/src/main/java/dk/catnip/android/android_app/MainActivity.java
+++ android-app/app/src/main/java/dk/catnip/android/android_app/MainActivity.java
@@ -1,13 +1,23 @@
 package dk.catnip.android.android_app;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.textView);
+    }
+
+    public void doStuff(View v) {
+        textView.setText("o shit waddup");
     }
 }
