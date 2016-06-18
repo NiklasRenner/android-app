0cbb80f35fb787fef0c04aab86868ffaed90621f
NiklasRenner <NiklasRennerNielsen@gmail.com>
2016-06-18 18:03:56 +0200

    fixed build

--- android-app/app/src/main/java/dk/catnip/android/android_app/MainActivity.java
+++ android-app/app/src/main/java/dk/catnip/android/android_app/MainActivity.java
@@ -3,11 +3,13 @@ package dk.catnip.android.android_app;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Switch;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView textView;
+    private Switch zwitch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,9 +17,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         textView = (TextView) findViewById(R.id.textView);
+        zwitch = (Switch) findViewById(R.id.switch1);
     }
 
     public void doStuff(View v) {
         textView.setText("o shit waddup");
+    }
+
+    public void doSwitch(View v) {
+        boolean isOn = zwitch.isChecked();
+
+        if (isOn) {
+            textView.setText("ON");
+        } else {
+            textView.setText("OFF");
+        }
+
     }
 }
