c422f0d717a69482073c2810c3a5b1dc230e0e61
dubidow <danielskaftenielsen@gmail.com>
2016-06-18 18:50:52 +0200

    Started quizvView
    and added buttons

--- android-app/app/src/main/java/dk/catnip/android/android_app/MainActivity.java
+++ android-app/app/src/main/java/dk/catnip/android/android_app/MainActivity.java
@@ -2,36 +2,37 @@ package dk.catnip.android.android_app;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Switch;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView textView;
-    private Switch zwitch;
+
+    private TextView questionText;
+    private Button buttonA;
+    private Button buttonB;
+    private Button buttonC;
+    private Button buttonD;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = (TextView) findViewById(R.id.textView);
-        zwitch = (Switch) findViewById(R.id.switch1);
+        buttonA = (Button) findViewById(R.id.button_a);
+        buttonB = (Button) findViewById(R.id.button_b);
+        buttonC = (Button) findViewById(R.id.button_c);
+        buttonD = (Button) findViewById(R.id.button_d);
+
+        questionText = (TextView) findViewById(R.id.text_question);
+
+        questionText.setText("7*2");
+
+        buttonA.setText("10");
+        buttonB.setText("22");
+        buttonC.setText("19");
+        buttonD.setText("14");
     }
 
-    public void doStuff(View v) {
-        textView.setText("o shit waddup");
-    }
-
-    public void doSwitch(View v) {
-        boolean isOn = zwitch.isChecked();
-
-        if (isOn) {
-            textView.setText("ON");
-        } else {
-            textView.setText("OFF");
-        }
-
-    }
 }
