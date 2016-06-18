82aa40cfbe52a3ca8ce4026c29ebc89b68d59631
NiklasRenner <NiklasRennerNielsen@gmail.com>
2016-06-19 01:56:05 +0200

    minor cleanup

--- android-app/app/src/main/java/dk/catnip/android/android_app/activities/MainActivity.java
+++ android-app/app/src/main/java/dk/catnip/android/android_app/activities/MainActivity.java
@@ -3,6 +3,7 @@ package dk.catnip.android.android_app.activities;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -25,9 +26,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //setup logo
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
-        getSupportActionBar().setLogo(R.drawable.ic_launcher);
-        getSupportActionBar().setDisplayUseLogoEnabled(true);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayShowHomeEnabled(true);
+            actionBar.setLogo(R.drawable.ic_launcher);
+            actionBar.setDisplayUseLogoEnabled(true);
+        }
 
         //get refs
         dao = new DataAccessor(getApplicationContext());
