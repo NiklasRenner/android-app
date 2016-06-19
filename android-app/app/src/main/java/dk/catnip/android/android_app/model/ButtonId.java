7a0c76fae8331390c6d2aad8e93680d34ba3506a
NiklasRenner <NiklasRennerNielsen@gmail.com>
2016-06-19 02:00:16 +0200

    updated button id enum

--- android-app/app/src/main/java/dk/catnip/android/android_app/model/ButtonId.java
+++ android-app/app/src/main/java/dk/catnip/android/android_app/model/ButtonId.java
@@ -1,11 +1,12 @@
 package dk.catnip.android.android_app.model;
 
 public enum ButtonId {
+
+    UNDEFINED(-1),
     A(0),
     B(1),
     C(2),
-    D(3),
-    UNDEFINED(-1);
+    D(3);
 
     int id;
 
